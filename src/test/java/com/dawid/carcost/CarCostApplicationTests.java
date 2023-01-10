6d8c2fae41b00a35424b8b3402733e4a69d84957
Dawid <dominiczeq@wp.pl>
2023-01-10 23:39:22 +0100

    Inital commit

---
+++ src/test/java/com/dawid/carcost/CarCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawid.carcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
